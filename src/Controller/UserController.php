720a093cd1564cd56011dad82eda796c0c2d66c2
Rémy Robin <remyrobin51@gmail.com>
2024-06-27 19:01:08 +0200

    :construction: WIP : Formulaire de création de compte.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 #[Route('/account', name: 'app_user')]
@@ -16,8 +20,22 @@ class UserController extends AbstractController
         ]);
     }
 
-    public function create(): Response
+    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, UserRepository $userRepository): Response
     {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form
+        ]);
     }
 }
