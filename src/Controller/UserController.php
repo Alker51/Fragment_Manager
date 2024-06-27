f6cbfe83048fcc739104c2dd2aeee70d98b2af86
Rémy Robin <remyrobin51@gmail.com>
2024-06-27 18:22:03 +0200

    :heavy_plus_sign: AJOUT : Création du controller User pour les futures routes.

---
+++ src/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+#[Route('/account', name: 'app_user')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: '_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+}
